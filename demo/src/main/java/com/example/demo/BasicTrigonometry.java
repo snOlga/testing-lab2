021b751ed21ee927812716a88557fe56f390f474
snOlga <olga.safonova@outlook.com>
2025-02-26 15:03:21 +0300

    full unmocked

--- demo/src/main/java/com/example/demo/BasicTrigonometry.java
+++ demo/src/main/java/com/example/demo/BasicTrigonometry.java
@@ -1,8 +1,22 @@
 package com.example.demo;
 
 public class BasicTrigonometry {
-    
-    public Double sin(Double x) {
-        return null;
+
+    private final int N = 10;
+
+    public double sin(double x) {
+        return solve(x, N);
+    }
+
+    private double solve(double x, int n) {
+        if (n < 0)
+            return 0;
+        return (Math.pow(-1.00, n) * Math.pow(x, (2 * n + 1)) / factorial(2 * n + 1)) + solve(x, n - 1);
+    }
+
+    private long factorial(int a) {
+        if (a <= 1)
+            return 1;
+        return a * factorial(a - 1);
     }
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -116,4 +116,23 @@ class DemoApplicationTests {
 
 		Assert.isTrue(result <= (expected + CALC_ERROR) && result >= (expected - CALC_ERROR), "");
 	}
+
+	@ParameterizedTest
+	@CsvSource({
+			"-3.14,0.00,1.00,0.00,0.00,0.00,0.00",
+			"2.00,0.00,0.00,0.69314718,0.69314718,1.60943791,0.6571538335"
+	})
+	void noMock(double x,
+			double mockSin,
+			double mockSec,
+			double mockLn,
+			double mockLn2,
+			double mockLn5,
+			double expected) {
+
+		FunctionSystem functionSystem = new FunctionSystem(new Trigonometry(new BasicTrigonometry()), new Logarithm(new BasicLogarithm()));
+		double result = functionSystem.count(x);
+
+		Assert.isTrue(result <= (expected + CALC_ERROR) && result >= (expected - CALC_ERROR), "");
+	}
 }
